44292840e5df399601541e25a2fb27c497500a1c
Brock Fanning <brockfanning@gmail.com>
2024-06-30 11:38:47 -0400

    Populate graph_title and indicator_available according to actual_indicator_available

--- scripts/alterations.py
+++ scripts/alterations.py
@@ -15,8 +15,14 @@ def alter_meta(meta, context):
         return re.sub(pattern, '', input_string)
 
     
+    # Remove the numbers from the global indicator/target/goal names.
     for key in ['SDG_GOAL__GLOBAL', 'SDG_TARGET__GLOBAL', 'SDG_INDICATOR__GLOBAL']:
         if key in meta:
             meta[key] = remove_global_numbers(meta[key])
 
+    # If "actual_indicator_available" is populated, use it for
+    # the indicator config settings: indicator_available and graph_title.
+    if 'actual_indicator_available' in meta and meta['actual_indicator_available'] != '':
+        meta['indicator_available'] = meta['actual_indicator_available']
+        meta['graph_title'] = meta['actual_indicator_available']
     return meta
