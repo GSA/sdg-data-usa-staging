c2e4be01d9a1cc5c2349fa4e27af1d29b494fd8f
btylerburton <104455281+btylerburton@users.noreply.github.com>
2024-08-14 22:05:03 +0000

    Deploying to gh-pages from  @ 14032bbdb84f6811a660bf0b7f50b055b6a18ae4 🚀

--- scripts/build_data.py
+++
@@ -1,5 +0,0 @@
-from sdg.open_sdg import open_sdg_build
-from alterations import alter_meta
-from alterations import alter_data
-
-open_sdg_build(config='config_data.yml', alter_meta=alter_meta, alter_data=alter_data)
